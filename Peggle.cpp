f990924ccd5f8722106cf9a65b1349c78757f791
Max <1430070@P-15.college.bart.qc.ca>
2015-06-16 08:57:29 -0400

    First commit

---
+++ Peggle.cpp
@@ -0,0 +1,32 @@
+
+#include "Peggle.h"
+
+Peggle::Peggle()
+{
+
+}
+
+Peggle::~Peggle()
+{
+
+}
+
+void Peggle::Start()
+{
+
+}
+
+void Peggle::Update()
+{
+	std::cout << "Test" << std::endl;
+}
+
+void Peggle::Draw()
+{
+
+}
+
+void Peggle::Stop()
+{
+
+}
